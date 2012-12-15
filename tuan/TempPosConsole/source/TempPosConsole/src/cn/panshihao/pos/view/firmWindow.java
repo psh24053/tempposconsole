ac427c3093e799a827557961ab2b0f291762ff32
shihao <shihao@panshihao>
2012-12-15 18:51:14 +0800

    商家管理还有很多问题

--- tuan/TempPosConsole/source/TempPosConsole/src/cn/panshihao/pos/view/firmWindow.java
+++ tuan/TempPosConsole/source/TempPosConsole/src/cn/panshihao/pos/view/firmWindow.java
@@ -12,12 +12,19 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import cn.panshihao.pos.dao.FirmDAO;
 import cn.panshihao.pos.handler.AsyncHandler;
 import cn.panshihao.pos.handler.ImageHandler;
 import cn.panshihao.pos.model.Firm;
+import cn.panshihao.pos.model.User;
 
 public class firmWindow extends superWindow {
 
@@ -36,7 +43,11 @@ public class firmWindow extends superWindow {
 	 */
 	public TabFolder firm_tab = null;
 	public TabItem firm_tab_all = null;
+	public TabItem firm_tab_add = null;
+	public TabItem firm_tab_modify = null;
 	
+	
+	public Table firm_tab_all_table = null;
 	
 	public firmWindow(superWindow parent) {
 		super(parent);
@@ -52,6 +63,8 @@ public class firmWindow extends superWindow {
 		
 		initTab();
 		
+		loadTabData();
+		
 		firm_shell.open();
 		firm_shell.layout();
 		
@@ -66,6 +79,44 @@ public class firmWindow extends superWindow {
 		firm_tab_all = new TabItem(firm_tab, SWT.NONE);
 		firm_tab_all.setText("全部商家");
 		
+	}
+	/**
+	 * 加载Tab数据
+	 */
+	private void loadTabData(){
+		
+		firm_tab_all_table = new Table(firm_tab, SWT.MULTI|SWT.FULL_SELECTION);
+		firm_tab_all_table.setHeaderVisible(true);
+		firm_tab_all_table.setLinesVisible(true);
+		
+		
+		TableColumn idColumn = new TableColumn(firm_tab_all_table, SWT.CENTER); 
+		idColumn.setText("ID");
+		idColumn.setWidth(marginWidthValue * 3);
+		
+		TableColumn nameColumn = new TableColumn(firm_tab_all_table, SWT.CENTER); 
+		nameColumn.setText("商家名称");
+		nameColumn.setWidth(marginWidthValue * 10);
+		
+		TableColumn descColumn = new TableColumn(firm_tab_all_table, SWT.CENTER); 
+		descColumn.setText("商家描述");
+		descColumn.setWidth(marginWidthValue * 18);
+		
+		TableColumn addressColumn = new TableColumn(firm_tab_all_table, SWT.CENTER); 
+		addressColumn.setText("商家地址");
+		addressColumn.setWidth(marginWidthValue * 15);
+		
+		TableColumn phoneColumn = new TableColumn(firm_tab_all_table, SWT.CENTER); 
+		phoneColumn.setText("联系电话");
+		phoneColumn.setWidth(marginWidthValue * 10);
+		
+		TableColumn personColumn = new TableColumn(firm_tab_all_table, SWT.CENTER); 
+		personColumn.setText("联系人");
+		personColumn.setWidth(marginWidthValue * 5);
+		
+		firm_tab_all.setControl(firm_tab_all_table);
+		
+		new loadFirmListAsyncHandler(This()).start("");
 	}
 	/**
 	 * 初始化按钮区域
@@ -96,17 +147,17 @@ public class firmWindow extends superWindow {
 	private void initBase(){
 		firm_shell = new Shell(getParent().getShell(), SWT.CLOSE | SWT.APPLICATION_MODAL);
 		firm_shell.setText(firm_title);
-		firm_shell.setSize(marginWidthValue * 50, marginHeightValue * 70);
+		firm_shell.setSize(marginWidthValue * 70, marginHeightValue * 70);
 		firm_shell.setLocation(getCenterX(firm_shell), getCenterY(firm_shell));
 	}
 	/**
 	 * 添加商家按钮的点击事件
 	 */
 	private void addFirm(){
-		if(firm_tab.getItemCount() == 1){
+		if(firm_tab_add == null){
 			// 创建添加商家选项卡
-			TabItem tab_add = new TabItem(firm_tab, SWT.NONE);
-			tab_add.setText("添加商家");
+			firm_tab_add = new TabItem(firm_tab, SWT.NONE);
+			firm_tab_add.setText("添加商家");
 			firm_tab.setSelection(1);
 			// 为选项卡增加内容
 			final Composite add_content = new Composite(firm_tab, SWT.NONE | SWT.WRAP);
@@ -160,12 +211,25 @@ public class firmWindow extends superWindow {
 				public void widgetSelected(SelectionEvent arg0) {
 					// TODO Auto-generated method stub
 					
-					validateAdd(add_content);
+					validateAdd(add_content, (Button) arg0.getSource());
 				}
 				
 			});
 			
-			tab_add.setControl(add_content);
+			Button cancel_button = new Button(add_content, SWT.NONE);
+			cancel_button.setText("取消添加");
+			cancel_button.setBounds(marginWidthValue * 35, add_button.getBounds().y + add_button.getBounds().height + marginHeightValue * 4, marginWidthValue * 10, marginHeightValue * 5);
+			
+			cancel_button.addSelectionListener(new SelectionAdapter() {
+				@Override
+				public void widgetSelected(SelectionEvent arg0) {
+					// TODO Auto-generated method stub
+					firm_tab_add.dispose();
+					firm_tab_add = null;
+				}
+			});
+			
+			firm_tab_add.setControl(add_content);
 		}
 		
 		
@@ -173,7 +237,7 @@ public class firmWindow extends superWindow {
 	/**
 	 * 验证添加商家
 	 */
-	private void validateAdd(Composite content){
+	private void validateAdd(Composite content, Button button){
 		Control[] children = content.getChildren();
 		
 		String nameValue = null;
@@ -236,8 +300,8 @@ public class firmWindow extends superWindow {
 			return;
 		}
 		
-		
-		new checkFirmNameExistAsyncHandler(This()).start("");
+		String[] values = {nameValue, descValue, addressValue, phoneValue, personValue};
+		new checkFirmNameExistAsyncHandler(This(), button).start(values);
 		
 	}
 	
@@ -248,23 +312,118 @@ public class firmWindow extends superWindow {
 		return firm_shell;
 	}
 	/**
+	 * 加载商家列表的asynchandler
+	 * @author shihao
+	 *
+	 */
+	private class loadFirmListAsyncHandler extends AsyncHandler<String, Integer, Firm[]>{
+
+		public loadFirmListAsyncHandler(superWindow window) {
+			super(window);
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		public Firm[] doInBackground(String... params) {
+			// TODO Auto-generated method stub
+			
+			FirmDAO dao = new FirmDAO();
+			JSONObject response = dao.getAllFirm(0, 1000);
+			try {
+				JSONArray list = response.getJSONArray("list");
+				Firm[] firms = new Firm[list.length()];
+				
+				for(int i = 0 ; i < list.length() ; i ++){
+					Firm f = new Firm();
+					JSONObject item = list.getJSONObject(i);
+					f.setFirm_address(item.getString("address"));
+					f.setFirm_desc(item.getString("desc"));
+					f.setFirm_id(item.getInt("fid"));
+					f.setFirm_name(item.getString("name"));
+					f.setFirm_person(item.getString("person"));
+					f.setFirm_phone(item.getString("phone"));
+					firms[i] = f;
+				}
+				
+				return firms;
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			return null;
+		}
+		
+		@Override
+		public void onComplete(Firm[] result) {
+			// TODO Auto-generated method stub
+			super.onComplete(result);
+			
+			if(result != null && result.length > 0){
+				loadFirmALL(result);
+			}
+			
+		}
+		
+	}
+	/**
+	 * 加载商家数据到界面
+	 * @param firms
+	 */
+	private void loadFirmALL(Firm[] firms){
+		
+		for(Firm f : firms){
+			TableItem item = new TableItem(firm_tab_all_table, SWT.NONE);
+			item.setData(f);
+			item.setText(new String[]{f.getFirm_id()+"",f.getFirm_name(),f.getFirm_desc(),f.getFirm_address(),f.getFirm_phone(),f.getFirm_person()});
+		}
+		
+	}
+	/**
 	 * 检查商家名称是否已存在
 	 * @author shihao
 	 *
 	 */
 	private class checkFirmNameExistAsyncHandler extends AsyncHandler<String, Integer, Boolean>{
 
-		public checkFirmNameExistAsyncHandler(superWindow window) {
+		private String[] values = null;
+		private Button button = null;
+		public checkFirmNameExistAsyncHandler(superWindow window, Button button) {
 			super(window);
 			// TODO Auto-generated constructor stub
+			this.button = button;
 		}
 
 		@Override
+		public void onBefore() {
+			// TODO Auto-generated method stub
+			button.setText("正在添加...");
+			button.setEnabled(false);
+			
+		}
+		@Override
 		public Boolean doInBackground(String... params) {
 			// TODO Auto-generated method stub
-			return null;
+			values = params;
+			
+			FirmDAO dao = new FirmDAO();
+			
+			return dao.checkFirmNameIsExist(values[0]);
 		}
-		
+		@Override
+		public void onComplete(Boolean result) {
+			// TODO Auto-generated method stub
+			super.onComplete(result);
+			
+			if(result){
+				new AddAsyncHandler(This(), button).start(values);
+			}else{
+				button.setText("确认添加");
+				button.setEnabled(true);
+				alert(getShell(), "添加错误", "商家名称已经存在！");
+			}
+			
+		}
 	}
 	/**
 	 * 执行添加商家操作的AsyncHandler
@@ -273,17 +432,48 @@ public class firmWindow extends superWindow {
 	 */
 	private class AddAsyncHandler extends AsyncHandler<String, Integer, Boolean>{
 
-		public AddAsyncHandler(superWindow window) {
+		private Button button;
+		
+		public AddAsyncHandler(superWindow window, Button button) {
 			super(window);
 			// TODO Auto-generated constructor stub
+			this.button = button;
 		}
 
 		@Override
 		public Boolean doInBackground(String... params) {
 			// TODO Auto-generated method stub
-			return null;
+			
+			FirmDAO dao = new FirmDAO();
+			
+			Firm firm = new Firm();
+			firm.setFirm_name(params[0]);
+			firm.setFirm_desc(params[1]);
+			firm.setFirm_address(params[2]);
+			firm.setFirm_phone(params[3]);
+			firm.setFirm_person(params[4]);
+			
+			User user = (User) cacheMap.get("curUser");
+			
+			return dao.addFirm(firm, user.getUser_id());
 		}
 		
+		@Override
+		public void onComplete(Boolean result) {
+			// TODO Auto-generated method stub
+			super.onComplete(result);
+			
+			if(result){
+				alert(getShell(), "添加成功", "添加成功！");
+				firm_tab_add.dispose();
+				firm_tab_add = null;
+			}else{
+				alert(getShell(), "添加错误", "添加错误！请稍后再试！");
+			}
+			button.setText("确认添加");
+			button.setEnabled(true);
+			
+		}
 	}
 
 }
